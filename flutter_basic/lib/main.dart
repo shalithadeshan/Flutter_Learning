c135d12ce72a1c2d3f4352742c6a04dd1a37fb97
shalithadeshan <shalithad8520jayasekara@gmail.com>
2021-05-26 21:13:07 +0530

    build widget tree, visible and invisible widget

--- flutter_basic/lib/main.dart
+++ flutter_basic/lib/main.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+// void main() {
+//   runApp(MyApp());
+// }
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello!'),);
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('The question!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: null,
+            ),
+          ],
+        ), // <why we use 'Widget' array holds list of widgets here>
+      ),
+    );
   }
 }
